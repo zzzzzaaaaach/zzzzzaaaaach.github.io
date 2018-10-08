b00dfab6479b29ff91923c8a7e66288844425a44
User <user@nicho135-30.user.msu.edu>
2018-10-08 16:45:14 -0400

    first upload of all web materials

---
+++ script.js
@@ -0,0 +1,39 @@
+function revealMessage() {
+	document.getElementById("hiddenMessage").style.display = 'block';
+}
+function revealMessage2(){
+	document.getElementById("hiddenMessage2").style.display = 'block';
+}
+function revealMessage3(){
+	document.getElementById("hiddenMessage3").style.display = 'block';
+}
+function revealMessage4(){
+	document.getElementById("hiddenMessage4").style.display = 'block';
+}
+function revealMessage5(){
+	document.getElementById("hiddenMessage5").style.display = 'block';
+}
+function revealMessage6(){
+	document.getElementById("hiddenMessage6").style.display = 'block';
+}
+function revealMessage7(){
+	document.getElementById("hiddenMessage7").style.display = 'block';
+}
+function revealMessage8(){
+	document.getElementById("hiddenMessage8").style.display = 'block';
+}
+function revealMessage9(){
+	document.getElementById("hiddenMessage9").style.display = 'block';
+}
+function revealMessage10(){
+	document.getElementById("hiddenMessage10").style.display = 'block';
+}
+function revealMessage11(){
+	document.getElementById("hiddenMessage11").style.display = 'block';
+}
+function revealMessage12(){
+	document.getElementById("hiddenMessage12").style.display = 'block';
+}
+function revealMessage13(){
+	document.getElementById("hiddenMessage13").style.display = 'block';
+}
